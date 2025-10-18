651fe3eabc3b72240aae6c4816e944979c18f5d3
Vitalii Kizlov <vitalii kizlov>
2025-10-18 12:54:47 +0200

    Initial Commit

---
+++ SailyTest/SailyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SailyTestApp.swift
+//  SailyTest
+//
+//  Created by Vitalii Kizlov on 18.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct SailyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
