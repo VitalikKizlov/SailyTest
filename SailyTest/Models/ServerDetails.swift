07d4c1b73619090ce73a5fc0ce5639a80b490b18
Vitalii Kizlov <vitalii kizlov>
2025-10-18 13:22:11 +0200

    Added network models

---
+++ SailyTest/Models/ServerDetails.swift
@@ -0,0 +1,13 @@
+//
+//  ServerDetails.swift
+//  SailyTest
+//
+//  Created by Vitalii Kizlov on 18.10.2025.
+//
+
+import Foundation
+
+struct ServerDetails: Codable {
+    let name: String
+    let distance: Int
+}
---
+++ SailyTest/Models/Token.swift
@@ -0,0 +1,12 @@
+//
+//  Token.swift
+//  SailyTest
+//
+//  Created by Vitalii Kizlov on 18.10.2025.
+//
+
+import Foundation
+
+struct Token: Codable {
+    let token: String
+}
