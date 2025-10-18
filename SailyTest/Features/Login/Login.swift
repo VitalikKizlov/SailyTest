46457dc86897c55c9d6958ed2fdb42aac57ba7a8
Vitalii Kizlov <vitalii kizlov>
2025-10-18 13:58:46 +0200

    Added Login reducer

---
+++ SailyTest/Features/Login/Login.swift
@@ -0,0 +1,41 @@
+//
+//  Login.swift
+//  SailyTest
+//
+//  Created by Vitalii Kizlov on 18.10.2025.
+//
+
+import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct Login {
+    enum Field: Hashable {
+        case username
+        case password
+    }
+
+    @ObservableState
+    struct State: Equatable {
+        var focus: Field?
+    }
+
+    enum Action: BindableAction, Equatable {
+        case didTapLoginButton
+        case binding(BindingAction<State>)
+    }
+
+    var body: some Reducer<State, Action> {
+        BindingReducer()
+
+        Reduce { state, action in
+
+            switch action {
+            case .didTapLoginButton:
+                return .none
+            case .binding(_):
+                return .none
+            }
+        }
+    }
+}
